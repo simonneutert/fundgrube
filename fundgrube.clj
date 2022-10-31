75c20916e595864460c25efa3eca6a5645097b48
Simon Neutert <simonneutert@users.noreply.github.com>
2022-10-31 19:52:32 +0100

    finish up with env vars

--- fundgrube.clj
+++ fundgrube.clj
@@ -4,42 +4,61 @@
 (require '[cheshire.core :as json])
 (require '[babashka.fs :as fs])
 
-;; SUPER DUPER CREEPY AND UGLY CODE STUB
+;; adjust to your needs, using the dev tools and inspecting the fundgrube
+(def url "https://www.mediamarkt.de/de/data/fundgrube/api/postings?limit=100&offset=0&outletIds=418&recentFilter=outlets")
+
+(def filename-current-results "data_fundgrube.edn")
+(def filename-past-results "data_fundgrube_old.edn")
 
 (defn pretty-spit [filename content]
   (spit filename (with-out-str (clojure.pprint/pprint content))))
 
 (defn get-json-data [url]
-  (-> (curl/get url
-                {:headers {"Accept" "application/json"
-                           "User-Agent" "Bacon/1.0"}})
+  (-> (curl/get url {:headers {"Accept" "application/json"
+                               "User-Agent" "Bacon/1.0"}})
       (get :body)
       (json/parse-string true)))
 
-(defn diff-results []
-  (let [old (edn/read-string (slurp "data_fundgrube_old.edn"))
-        new (edn/read-string (slurp "data_fundgrube.edn"))]
+(defn diff-fundgrube-results []
+  (let [old (edn/read-string (slurp filename-past-results))
+        new (edn/read-string (slurp filename-current-results))]
     (cset/difference (set (keys new)) (set (keys old)))))
 
-(if (fs/exists? "data_fundgrube.edn")
-  (fs/move "data_fundgrube.edn" "data_fundgrube_old.edn" {:replace-existing true})
-  (spit "data_fundgrube_old.edn" {}))
+(defn product->text [product]
+  (str (:name product) "\n\n"
+       "Preis: " (:price product) " €"
+       "\n\n"
+       (:posting_text product) "\n"
+       (first (:original_url product))))
 
-(def url "https://www.mediamarkt.de/de/data/fundgrube/api/postings?limit=100&offset=0&outletIds=418&recentFilter=outlets")
+(defn tgram-url []
+  (str "https://api.telegram.org/bot"
+       (System/getenv "FUNDGRUBE_TGRAM_API_KEY")
+       "/sendMessage"))
 
-(let [json-data (get-json-data url)]
-  ;; persist
-  (->> (reduce #(assoc %1 (:name %2) %2) {} (:postings json-data))
-       (pretty-spit "data_fundgrube.edn"))
-  ;; make diff
-  (if (= (count (set (keys (reduce #(assoc %1 (:name %2) %2) {} (:postings json-data)))))
-         (count (diff-results)))
+(defn json-content [product]
+  (json/encode {"chat_id" (System/getenv "FUNDGRUBE_TGRAM_CHANNEL")
+                "text" (product->text product)}))
+
+(defn send-to-tgram [fundgrube-current diff-result]
+  (for [result diff-result]
+    (let [product (get fundgrube-current result)
+          url (tgram-url)]
+      (curl/post url {:headers {"Content-Type" "application/json"}
+                      :body (json-content product)}))))
+
+(defn build-file-structure [json-data]
+  (let [fundgrube-current (reduce #(assoc %1 (:name %2) %2) {} (:postings json-data))]
+    (pretty-spit filename-current-results fundgrube-current)
+    fundgrube-current))
+
+(if (fs/exists? filename-current-results)
+  (fs/move filename-current-results filename-past-results {:replace-existing true})
+  (spit filename-past-results {}))
+
+(let [json-data (get-json-data url)
+      fundgrube-current (build-file-structure json-data)
+      diff-result (diff-fundgrube-results)]
+  (if (= (count (set (keys fundgrube-current))) (count diff-result))
     (prn "done")
-    (for [result (diff-results)]
-      (let [product (get (edn/read-string (slurp "data_fundgrube.edn")) result)]
-        ;; TODO: include call to apprise with secret as ENV
-        (prn product)))))
-
-
-
-
+    (send-to-tgram fundgrube-current diff-result)))
